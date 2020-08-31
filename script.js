b9a65405868b0f2996ce5bcac17d15bc923ee26a
programmertaha <programmer.taha.1y@gmail.com>
2020-08-31 11:01:32 +0300

    #7 task

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ I will be easy Taha.
 */
 
 //let the user put his/her name
-let userName = '';
+let userName = "";
 
 //decides what to do if the user enters a name or not
 userName ? console.log(`Hello, ${userName}!`) : console.log(`Hello!`);
 
 //the question the user wants to ask the Magic Eight Ball
-let userQuestion = '';
+let userQuestion = "";
 
 //logging
 console.log(`${userName} asks: ${userQuestion}`);
@@ -20,5 +20,55 @@ console.log(`${userName} asks: ${userQuestion}`);
 let randomNumber = Math.floor(Math.random() * 8);
 
 //
-let eightBall = '';
+let eightBall = "";
+
+//create a control flow that takes in the randomNumber
+//and then assigns eightBall to a reply that a Magic Eight Ball would return
+/*
+if (randomNumber === 0) {
+  console.log("It is certain");
+} else if (randomNumber === 1) {
+  console.log("It is decidedly so");
+} else if (randomNumber === 2) {
+  console.log("Reply hazy try again");
+} else if (randomNumber === 3) {
+  console.log("Cannot predict now");
+} else if (randomNumber === 4) {
+  console.log("Do not count on it");
+} else if (randomNumber === 5) {
+  console.log("My sources say no");
+} else if (randomNumber === 6) {
+  console.log("Outlook not so good");
+} else if (randomNumber === 7) {
+  console.log("Signs point to yes");
+}
+// */
+/*
+switch (randomNumber) {
+  case 0:
+    console.log("It is certain");
+    break;
+  case 1:
+    console.log("It is decidedly so");
+    break;
+  case 2:
+    console.log("Reply hazy try again");
+    break;
+  case 3:
+    console.log("Cannot predict now");
+    break;
+  case 4:
+    console.log("Do not count on it");
+    break;
+  case 5:
+    console.log("My sources say no");
+    break;
+  case 6:
+    console.log("Outlook not so good");
+    break;
+  case 7:
+    console.log("Signs point to yes");
+    break;
+}
+*/
 
